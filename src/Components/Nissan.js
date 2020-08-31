b3fcd5d5aa8ebfad728001e6fff1807f679dbec6
Dino Cajic <dinocajic@gmail.com>
2020-08-31 13:58:16 -0400

    3 Functional Components

---
+++ src/Components/Nissan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nissan() {
+
+    return(
+        <div>
+            1990 Nissan 240sx
+        </div>
+    );
+}
+
+export default Nissan;
---
+++ src/Components/Toyota.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Toyota() {
+
+    return(
+        <div>
+            1995 Toyota Supra
+        </div>
+    );
+}
+
+export default Toyota;
